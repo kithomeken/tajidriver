69e19f6ae7dcfff732916524413b445124995b3d
Kennedy Kithome <kithomeken@gmail.com>
2020-03-26 15:17:24 +0300

    Contact Us

--- app/src/main/java/com/tajidriver/settings/ContactUs.java
+++ app/src/main/java/com/tajidriver/settings/ContactUs.java
@@ -1,10 +1,13 @@
 package com.tajidriver.settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.tajidriver.R;
+import com.tajidriver.global.Variables;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -12,5 +15,27 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Variables.ACTIVITY_STATE = 0;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Variables.ACTIVITY_STATE = 0;
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
