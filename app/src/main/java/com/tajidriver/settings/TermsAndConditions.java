5e9f8af5529072d3ac60d83b0ecf90f5fe774bec
Kennedy Kithome <kithomeken@gmail.com>
2020-04-06 00:12:24 +0300

    Terms And Conditions

--- app/src/main/java/com/tajidriver/settings/TermsAndConditions.java
+++ app/src/main/java/com/tajidriver/settings/TermsAndConditions.java
@@ -1,10 +1,14 @@
 package com.tajidriver.settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
 
 import com.tajidriver.R;
+import com.tajidriver.global.Variables;
 
 public class TermsAndConditions extends AppCompatActivity {
 
@@ -12,5 +16,30 @@ public class TermsAndConditions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/terms_and_conditions.html");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Variables.ACTIVITY_STATE = 0;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Variables.ACTIVITY_STATE = 0;
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
