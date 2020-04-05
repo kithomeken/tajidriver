245ef6cf76a04c1dbd2ce20b9586319c7a4bc6b3
Kennedy Kithome <kithomeken@gmail.com>
2020-04-06 00:13:05 +0300

    On Boarding UI Permissions Get Access

--- app/src/main/java/com/tajidriver/app/OnBoardingUI.java
+++ app/src/main/java/com/tajidriver/app/OnBoardingUI.java
@@ -1,8 +1,13 @@
 package com.tajidriver.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +17,8 @@ import com.tajidriver.global.Variables;
 import com.tajidriver.home.Home;
 
 public class OnBoardingUI extends AppCompatActivity {
+    protected static final int REQUEST_PHONE_CALL = 0x1;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +39,25 @@ public class OnBoardingUI extends AppCompatActivity {
                 }
             }
         });
+
+        permissionCheck();
+    }
+
+    private void permissionCheck() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(OnBoardingUI.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_CALL);
+            }
+
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
     }
 }
