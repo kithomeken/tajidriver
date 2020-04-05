23e748866b639c9e4e5ff7aa4b0eca8c18407ef8
Kennedy Kithome <kithomeken@gmail.com>
2020-04-06 00:11:40 +0300

    Authentication Fix

---
+++ app/src/main/java/com/tajidriver/settings/TermsAndConditions.java
@@ -0,0 +1,16 @@
+package com.tajidriver.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tajidriver.R;
+
+public class TermsAndConditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_and_conditions);
+    }
+}
